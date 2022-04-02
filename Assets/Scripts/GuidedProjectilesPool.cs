c1009a393694c703ed763807176ddc6249666bd6
ioilcon <Zdvadim98@gmail.com>
2022-04-03 00:24:45 +0300

    Пул наводящихся снарядов

---
+++ Assets/Scripts/GuidedProjectilesPool.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuidedProjectilesPool : MonoBehaviour
+{
+    [SerializeField] private GuidedProjectile projectilePrefab;
+    [SerializeField] private int poolSize = 10;
+    [SerializeField] private bool autoExpand;
+
+    public ObjectPool<GuidedProjectile> Pool { get; private set; }
+
+    private void Start()
+    {
+        Pool = new ObjectPool<GuidedProjectile>(projectilePrefab, poolSize, autoExpand, transform);
+    }
+}

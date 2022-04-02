e2b0adde2a8080210ac9428d10aced82a17aa686
ioilcon <Zdvadim98@gmail.com>
2022-04-02 19:25:47 +0300

    Пул объектов для работы со снарядами башен

---
+++ Assets/Scripts/ObjectPool.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+public class ObjectPool<T> where T : MonoBehaviour
+{
+    private T Prefab { get; }
+    private bool AutoExpand { get; set; }
+    private Transform Container {get; }
+    
+    private List<T> _pool;
+    
+    public ObjectPool(T prefab, int poolSize, Transform container = null)
+    {
+        this.Prefab = prefab;
+        this.Container = container;
+        
+        this.CreatePool(poolSize);
+    }
+
+    private void CreatePool(int poolSize)
+    {
+        this._pool = new List<T>();
+        for (var i = 0; i < poolSize; ++i)
+        {
+            this.CreateObject();
+        }
+    }
+
+    private T CreateObject(bool isActiveByDefault = false)
+    {
+        var createdObject = Object.Instantiate(this.Prefab, this.Container);
+        createdObject.gameObject.SetActive(isActiveByDefault);
+        this._pool.Add(createdObject);
+        return createdObject;
+    }
+
+    private bool HasFreeElement(out T element)
+    {
+        foreach (var obj in _pool.Where(obj => !obj.gameObject.activeInHierarchy))
+        {
+            obj.gameObject.SetActive(true);
+            element = obj;
+            return true;
+        }
+
+        element = null;
+        return false;
+    }
+
+    public T GetFreeElement()
+    {
+        if (this.HasFreeElement(out var element))
+            return element;
+        if (this.AutoExpand)
+            return this.CreateObject(true);
+        
+        throw new Exception($"There are no such {typeof(T)} objects in pool");
+    }
+    
+}
